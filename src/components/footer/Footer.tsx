e0659cc1e771b71c0a1e1c81a90477aa6efd0808
gtoootg0131 <gotosan0131@yahoo.co.jp>
2022-05-01 16:54:16 +0200

    update

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Icon } from '../header/Header';
+
+import ContactForm from './ContactForm';
+
+import styles from './Footer.module.scss'
+
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+import { useTranslation } from 'react-i18next';
+
+function FooterLeft(){
+    return(
+        <div className={styles.footer_left}>
+            <ContactForm/>
+        </div>
+    )
+}
+
+function FooterRight(){
+
+  const {t} = useTranslation()
+
+  const icons = [
+    {
+      href: 'https://github.com/gtoootg0131',
+      icon: faGithub,
+    },
+    {
+      href: 'https://www.linkedin.com/in/%E5%80%AB%E5%AE%8F-%E5%BE%8C%E8%97%A4-37b905218/',
+      icon: faLinkedinIn,
+    },
+  ]
+
+  const profiles: string[] = [
+    t<string>('footer.profile1'),
+    t<string>('footer.profile2'),
+    t<string>('footer.profile3'),
+    t<string>('footer.profile4'),
+  ]
+
+    return(
+        <div className={styles.footer_right}>
+          <div className={styles.footer_right_text}>
+            <h2>{t<string>('footer.name')}</h2>
+            {profiles.map((profile,i) => <h4>{profile}</h4>)}
+          </div>
+          <div className={styles.footer_right_icons}>
+            {icons.map((icon,i) => <Icon icon={icon.icon} href={icon.href} color="#2a3139" fontSize={35}/>)}
+          </div>
+        </div>
+    )
+}
+
+function Footer(){
+    return (
+          <div className={styles.footer}>
+            <FooterLeft/>
+            <FooterRight/>
+          </div>
+    );
+}
+
+export default Footer;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,9 +2,33 @@ import classes from './Header.module.scss'
 import { useTranslation } from 'next-i18next'
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faLinkedinIn, IconDefinition } from '@fortawesome/free-brands-svg-icons'
+import { useRouter } from 'next/router'
+
+
+interface NavigationIconProps{
+  icon: IconDefinition,
+  href: string,
+  fontSize: number,
+  color: string
+}
+
+export function Icon({icon,href,fontSize,color}: NavigationIconProps) {
+ 
+  return (
+          <div className={classes.header_container_navigation_icon}>
+            <Link href={href}>
+              <FontAwesomeIcon
+                icon={icon}
+                style={{ fontSize: fontSize, color: color }}
+              />
+            </Link>
+          </div>
+  )
+}
 export default function Header() {
   const { t } = useTranslation()
+  const {route, locale} = useRouter()
 
   function NavigationText() {
     const textItems = [
@@ -17,12 +41,18 @@ export default function Header() {
         href: '/profile',
       },
       {
-        text: t('header.navigation.profile'),
+        text: t('header.navigation.portfolio'),
         href: 'https://goto-travels.herokuapp.com/',
       },
     ]
+
     return (
       <>
+        <div className={classes.header_container_navigation_text}>
+          <Link href={locale ==='en'? `/${route}`: `/en/${route}`} >
+            <h3>{locale ==='en'? '日本語': 'EN'}</h3>
+          </Link>
+        </div>
         {textItems.map((textItem, index) => {
           return (
             <div className={classes.header_container_navigation_text}>
@@ -32,40 +62,21 @@ export default function Header() {
             </div>
           )
         })}
+ 
       </>
     )
   }
 
-  function NavigationIcon() {
-    const icons = [
-      {
-        href: 'https://github.com/gtoootg0131',
-        icon: faGithub,
-      },
-      {
-        href: 'https://www.linkedin.com/in/%E5%80%AB%E5%AE%8F-%E5%BE%8C%E8%97%A4-37b905218/',
-        icon: faLinkedinIn,
-      },
-    ]
-
-    return (
-      <>
-        {icons.map((icon, index) => {
-          return (
-            <div className={classes.header_container_navigation_icon}>
-              <Link key={index} href={icon.href}>
-                <FontAwesomeIcon
-                  icon={icon.icon}
-                  style={{ fontSize: 50, color: 'white' }}
-                />
-              </Link>
-            </div>
-          )
-        })}
-      </>
-    )
-  }
-
+  const icons = [
+    {
+      href: 'https://github.com/gtoootg0131',
+      icon: faGithub,
+    },
+    {
+      href: 'https://www.linkedin.com/in/%E5%80%AB%E5%AE%8F-%E5%BE%8C%E8%97%A4-37b905218/',
+      icon: faLinkedinIn,
+    },
+  ]
   return (
     <div className={classes.header}>
       <div className={classes.header_container}>
@@ -74,7 +85,7 @@ export default function Header() {
         </div>
         <div className={classes.header_container_navigation}>
           <NavigationText />
-          <NavigationIcon />
+          {icons.map((icon,map) => <Icon icon={icon.icon} href={icon.href} color="white" fontSize={35}/>)}
         </div>
       </div>
     </div>
