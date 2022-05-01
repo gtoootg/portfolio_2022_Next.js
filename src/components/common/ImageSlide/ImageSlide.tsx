569f15415b9149a4aa768c59c101c03d1a851e92
gtoootg0131 <gotosan0131@yahoo.co.jp>
2022-05-01 14:14:55 +0200

    initial commit

--- src/components/common/ImageSlide/ImageSlide.tsx
+++ src/components/common/ImageSlide/ImageSlide.tsx
@@ -1,36 +1,39 @@
-import { useTransition, animated} from '@react-spring/web'
 import { StaticImageData } from 'next/image'
 import Image from 'next/image'
-import croatia from '/public/assets/photo/croatia.jpg'
-// interface ImageSlideProps {
-//   images: StaticImageData[],
-//   index : number
 
-// }
+import {useState, useEffect} from 'react'
 
-export default function ImageSlide({images,index}) {
+import styles from './ImageSlide.module.scss'
 
-  const transitions = useTransition(index, {
-    key: index,
-    from: { opacity: 0 },
-    enter: { opacity: 0.9 },
-    leave: { opacity: 0 },
-    config: { duration: 300 }
-  })
+interface ImageSlideProps {
+  images: StaticImageData[] | string[]
+}
 
+export default function ImageSlide({images}: ImageSlideProps) {
+  const [index, setIndex] = useState(0)
+  const setIndexHandler = ()=>{
+    setIndex(prev=> (prev + 1) % (images.length))
+  }
+  useEffect(() => {
+    const interval = setInterval(
+      setIndexHandler
+    , 6000);
+    return () => clearInterval(interval);
+  },[])
+ 
   return (
     <div>
-      {transitions((style, index) => (
-          <animated.img 
-              src={images[index]}
-              style={{
-                ...style,
-                width:"100%",
-                // height:"auto",
-                position:"absolute"      
-            }}
-          />      
-      ))}
-    </div>
+    {images.map((image, i)=>{
+        return(
+            <div 
+              className={ i ===index? styles.image_fadeIn: styles.image_fadeOut} 
+              key={i} 
+            >
+             <Image src={image} width={3} height ={2} layout="responsive"  priority loading='eager'/>
+            </div>
+        )
+      }
+    )}
+   </div>
   )
 }
